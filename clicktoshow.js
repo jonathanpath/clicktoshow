92974551040f296698900ee101eb1db8cd2acc8a
Jonathan Path <contact@jonathanpath.com>
2015-05-06 15:17:11 +0200

    Add slides effects

--- clicktoshow.js
+++ clicktoshow.js
@@ -27,12 +27,47 @@
         tohide.fadeOut(settings.duration, function(){
           toshow.fadeIn(settings.duration);
         });
+      } else if(settings.effect == "slideUp") {
+        tohide.animate({
+          'top': '-500px',
+          'opacity': '0'
+        }, settings.duration, function(){
+          $(this).hide();
+          toshow.css({
+            display: 'block',
+            top: '+500px',
+            opacity: 0
+          });
+          toshow.animate({
+            top: 0,
+            opacity: 1
+          });
+        });
+      } else if(settings.effect == "slideBottom") {
+        tohide.animate({
+          'top': '500px',
+          'opacity': '0'
+        }, settings.duration, function(){
+          $(this).hide();
+          toshow.css({
+            display: 'block',
+            top: '-500px',
+            opacity: 0
+          });
+          toshow.animate({
+            top: 0,
+            opacity: 1
+          });
+        });
       }
 
       // nav
       if(settings.nav == true) {
-        navTohide.addClass('is-done');
-        navToshow.addClass('is-current');
+        $('[data-toshow-nav]')
+          .removeClass('is-current');
+        navToshow
+          .addClass('is-current')
+          .addClass('is-done');
       }
 
       return false;
