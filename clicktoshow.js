12acc35a88c0b7fd1f6c517986e98725dce2cb98
Jonathan Path <contact@jonathanpath.com>
2015-05-06 14:29:29 +0200

    Add effect, nav and duration options

--- clicktoshow.js
+++ clicktoshow.js
@@ -3,12 +3,41 @@
  * Copyright 2015 Jonathan Path
  */
 
-$.fn.clicktoshow = function() {
-  $(this).click(function(){
-    $('#'+$(this).data("toshow")).show();
-    $('#'+$(this).data("toshow2")).show();
-    $('#'+$(this).data("tohide")).hide();
-    $('#'+$(this).data("tohide2")).hide();
-    return false;
-  });
-};
+(function ( $ ) {
+
+  $.fn.clicktoshow = function( options ) {
+
+    var settings = $.extend({
+        effect: "none",
+        duration: 300,
+        nav: false
+    }, options );
+
+    $(this).bind("click", function(){
+      var navToshow = $('#'+$(this).data("toshow")+'-nav');
+      var navTohide = $('#'+$(this).data("tohide")+'-nav');
+      var toshow = $('#'+$(this).data("toshow")+',#'+$(this).data("toshow2"));
+      var tohide = $('#'+$(this).data("tohide")+',#'+$(this).data("tohide2"));
+
+      // effect
+      if(settings.effect == "none") {
+        toshow.show();
+        tohide.hide();
+      } else if(settings.effect == "fade") {
+        tohide.fadeOut(settings.duration, function(){
+          toshow.fadeIn(settings.duration);
+        });
+      }
+
+      // nav
+      if(settings.nav == true) {
+        navTohide.addClass('is-done');
+        navToshow.addClass('is-current');
+      }
+
+      return false;
+    });
+
+  };
+
+}( jQuery ));
